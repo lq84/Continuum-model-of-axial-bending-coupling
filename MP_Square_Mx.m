06beca8f7bab2d99812e1c589c7ddb3a15522ad7
Jim <40237951+lq84@users.noreply.github.com>
2021-11-20 15:25:21 +0800

    Update MP_Square_Mx.m

--- MP_Square_Mx.m
+++ MP_Square_Mx.m
@@ -8,11 +8,11 @@ theta=deg2rad(theta);
 type=1; % 1 for achiral, 2 for chiral
 
 %% Material properties
-E=26;                                                                    % Young's modulus (MPa)
-G=27;                                                                    % Shear modulus
+E=26e6;                                                                   % Young's modulus (MPa)
+G=27e6;                                                                    % Shear modulus
 v=0.3;                                                                     % Poisson's ratio
 % ro=2700;                                                                   % Density (kg/m^3)
-h=36;                                                                       % Beam height
+h=1;                                                                       % Beam height
 Iz=h*t^3/12;                                                               % Moment of inertia (m^4)
 A=t*h;                                                                     % Cross section area (m^2)
 
@@ -30,41 +30,6 @@ K_elm=[A*E/2/a0,             0,             0, -A*E/2/a0,             0,        
           0, -3*E*Iz/2/a0^3, -3*E*Iz/2/a0^2,        0,  3*E*Iz/2/a0^3, -3*E*Iz/2/a0^2;
           0,  3*E*Iz/2/a0^2,        E*Iz/a0,        0, -3*E*Iz/2/a0^2,     2*E*Iz/a0];           % Euler beam
 
-% s=4;c=1/2;
-% s1=s*(1+c);s2=2*s1;
-% k_elm1=E*t/Lelm*[1,0,0,-1,0,0;
-%     0,0,0,0,0,0;
-%     0,0,0,0,0,0;
-%     -1,0,0,1,0,0;
-%     0,0,0,0,0,0;
-%     0,0,0,0,0,0];
-% k_elm2=E*t^3/Lelm/12*[0,0,0,0,0,0;
-%     0,s2/Lelm^2,s1/Lelm,0,-s2/Lelm^2,s1/Lelm;
-%     0,s1/Lelm,s,0,-s1/Lelm,s*c;
-%     0,0,0,0,0,0;
-%     0,-s2/Lelm^2,-s1/Lelm,0,s2/Lelm^2,-s1/Lelm;
-%     0,s1/Lelm,s*c,0,-s1/Lelm,s];
-% K_elm=k_elm1*h+k_elm2*h;
-
-% K_elm=(E*h*t)/Lelm*[1,           0,               0,   -1,                   0,               0;
-%              0,    (12*t^2)/Lelm^2,    (6*t^2)/Lelm,    0,    -(12*t^2)/Lelm^2,    (6*t^2)/Lelm;
-%              0,       (6*t^2)/Lelm,           4*t^2,    0,       -(6*t^2)/Lelm,           2*t^2;
-%             -1,                  0,               0,    1,                   0,               0;
-%              0,   -(12*t^2)/Lelm^2,   -(6*t^2)/Lelm,    0,     (12*t^2)/Lelm^2,   -(6*t^2)/Lelm;
-%              0,       (6*t^2)/Lelm,           2*t^2,    0,       -(6*t^2)/Lelm,           4*t^2];
-%       
-
-
-% A0=10*(1+v)/(12+11*v)*A;                                                   % Shear correction factor
-% s=(4*Lelm^2/3+4*E*Iz/G/A0)/(Lelm^2/3+2*E*Iz/G/A0);
-% c=(2*Lelm^2/3-4*E*Iz/G/A0)/(4*Lelm^2/3+4*E*Iz/G/A0);
-% s1=s*(1+c);s2=2*s1;
-% K_elm=E*Iz/Lelm*[12/t^2,             0,             0, -12/t^2,             0,             0;
-%                 0,        s2/Lelm^2,          s1/Lelm,        0,      -s2/Lelm^2,          s1/Lelm;
-%                 0,          s1/Lelm,             s,        0,        -s1/Lelm,           s*c;
-%           -12/t^2,             0,             0,  12/t^2,             0,             0;
-%                 0,       -s2/Lelm^2,         -s1/Lelm,        0,       s2/Lelm^2,         -s1/Lelm;
-%                 0,          s1/Lelm,           s*c,        0,        -s1/Lelm,            s];           % Timoshenko beam
 
 %% K_OA
 ori=zeros(n,1);
@@ -144,7 +109,7 @@ K_OB(abs(K_OB)<1e-3)=0;
 
 
 %% K_OABCD
-% beam orientation, [OA, OC]
+% beam orientation, [OB, OD]
 ori=[0, 0];
 % connection list, OABCD=1,2,3,4,5
 cnct=[1,2; 4,1;];
@@ -173,9 +138,9 @@ for i=1:length(ori)
 end                                                                        % Diagonized Stiffness & Mass matrix (not assembled)
 
 %% Beams on the surface
-% beam orientation, [OB, OD]
+% beam orientation, [OA, OC]
 ori=[pi/2, pi/2];
-% connection list, OABCDEF=1,2,3,4,5,6,7
+% connection list, OABCD=1,2,3,4,5
 cnct=[1,3; 5,1;];
 for i=1:length(ori)
     T=[cos(ori(i)), sin(ori(i)), 0, 0, 0, 0;
@@ -203,10 +168,6 @@ end                                                                        % Dia
 %% Global stiffness & mass matrix: K_OABCDEF
 K_OAtoD(abs(K_OAtoD)<1e-3)=0;
 
-% Kuu=K_OAtoD(1:3,1:3); Kuk=K_OAtoD(1:3,4:end);
-% Kp=-Kuu\Kuk;
-% Kp(abs(Kp)<1e-3)=0;
-
 aa=K_OAtoD(1:3,1:3);
 ab=K_OAtoD(1:3,4:end);
 ba=K_OAtoD(4:end,1:3);
@@ -218,73 +179,30 @@ Lxy=a;
 V=2*a^2;
 ori=[0,pi/2,pi,3/2*pi];
 syms e11 e22 e12 e21 k13 k23 phi0 u0x u0y real
-u12=e21-phi0;
-u21=e12+phi0;
-phi1=phi0+(k13*Lxy*cos(ori(1))+k23*Lxy*sin(ori(1)));
-phi2=phi0+(k13*Lxy*cos(ori(2))+k23*Lxy*sin(ori(2)));
-phi3=phi0+(k13*Lxy*cos(ori(3))+k23*Lxy*sin(ori(3)));
-phi4=phi0+(k13*Lxy*cos(ori(4))+k23*Lxy*sin(ori(4)));
-u1x=e11*Lxy*cos(ori(1))+u12*Lxy*sin(ori(1));
-u1y=e22*Lxy*sin(ori(1))+u21*Lxy*cos(ori(1));
-u2x=e11*Lxy*cos(ori(2))+u12*Lxy*sin(ori(2));
-u2y=e22*Lxy*sin(ori(2))+u21*Lxy*cos(ori(2));
-u3x=e11*Lxy*cos(ori(3))+u12*Lxy*sin(ori(3));
-u3y=e22*Lxy*sin(ori(3))+u21*Lxy*cos(ori(3));
-u4x=e11*Lxy*cos(ori(4))+u12*Lxy*sin(ori(4));
-u4y=e22*Lxy*sin(ori(4))+u21*Lxy*cos(ori(4));
-% u1x=e11*Lxy*cos(ori(1))+u12*Lxy*sin(ori(1))-k23/2*(Lxy*sin(ori(1)))^2-k13*Lxy^2*sin(ori(1))*cos(ori(1));
-% u1y=e22*Lxy*sin(ori(1))+u21*Lxy*cos(ori(1))+k13/2*(Lxy*cos(ori(1)))^2+k23*Lxy^2*sin(ori(1))*cos(ori(1));
-% u2x=e11*Lxy*cos(ori(2))+u12*Lxy*sin(ori(2))-k23/2*(Lxy*sin(ori(2)))^2-k13*Lxy^2*sin(ori(2))*cos(ori(2));
-% u2y=e22*Lxy*sin(ori(2))+u21*Lxy*cos(ori(2))+k13/2*(Lxy*cos(ori(2)))^2+k23*Lxy^2*sin(ori(2))*cos(ori(2));
-% u3x=e11*Lxy*cos(ori(3))+u12*Lxy*sin(ori(3))-k23/2*(Lxy*sin(ori(3)))^2-k13*Lxy^2*sin(ori(3))*cos(ori(3));
-% u3y=e22*Lxy*sin(ori(3))+u21*Lxy*cos(ori(3))+k13/2*(Lxy*cos(ori(3)))^2+k23*Lxy^2*sin(ori(3))*cos(ori(3));
-% u4x=e11*Lxy*cos(ori(4))+u12*Lxy*sin(ori(4))-k23/2*(Lxy*sin(ori(4)))^2-k13*Lxy^2*sin(ori(4))*cos(ori(4));
-% u4y=e22*Lxy*sin(ori(4))+u21*Lxy*cos(ori(4))+k13/2*(Lxy*cos(ori(4)))^2+k23*Lxy^2*sin(ori(4))*cos(ori(4));
+u0x=0; u0y=0;
+u12=(e21+e12)/2;
+u21=(e21+e12)/2;
+phi0=(e21-e12)/2; % Omega==0
+psi0=phi0;
+psi1=psi0+(k13*Lxy*cos(ori(1))+k23*Lxy*sin(ori(1)));
+psi2=psi0+(k13*Lxy*cos(ori(2))+k23*Lxy*sin(ori(2)));
+psi3=psi0+(k13*Lxy*cos(ori(3))+k23*Lxy*sin(ori(3)));
+psi4=psi0+(k13*Lxy*cos(ori(4))+k23*Lxy*sin(ori(4)));
+u1x=u0x+e11*Lxy*cos(ori(1))+u12*Lxy*sin(ori(1));
+u1y=u0y+e22*Lxy*sin(ori(1))+u21*Lxy*cos(ori(1));
+u2x=u0x+e11*Lxy*cos(ori(2))+u12*Lxy*sin(ori(2));
+u2y=u0y+e22*Lxy*sin(ori(2))+u21*Lxy*cos(ori(2));
+u3x=u0x+e11*Lxy*cos(ori(3))+u12*Lxy*sin(ori(3));
+u3y=u0y+e22*Lxy*sin(ori(3))+u21*Lxy*cos(ori(3));
+u4x=u0x+e11*Lxy*cos(ori(4))+u12*Lxy*sin(ori(4));
+u4y=u0y+e22*Lxy*sin(ori(4))+u21*Lxy*cos(ori(4));
 
 
-Mtt=0;
-xk=Lxy*[cos(ori);sin(ori)];
-xk(abs(xk)<1e-3)=0;
-dk=[u1x;u1y;phi1;u2x;u2y;phi2;u3x;u3y;phi3;u4x;u4y;phi4;];
-for k=1:length(ori)
-    for i=1:2
-        for j=1:2
-            if i==j
-                eijk=0;
-            elseif i>j
-                eijk=1;
-                vpa(eijk*K_AtoD(3*(k-1)+i,:)*dk*abs(xk(j,k)),4);
-            else
-                eijk=-1;
-                vpa(eijk*K_AtoD(3*(k-1)+i,:)*dk*abs(xk(j,k)),4);
-            end
-            Mtt=Mtt+eijk*K_AtoD(3*(k-1)+i,:)*dk*abs(xk(j,k)); % 2016 Waas, eq13d
-        end
-    end
-    Mtt=Mtt+K_AtoD(3*(k-1)+3,:)*dk;
-end
-s2=vpasolve(Mtt==0,phi0);
-sw0=vpa(s2,4);
-
-
-p0=subs(phi0,phi0,sw0);
-p1=subs(phi1,phi0,sw0);
-p2=subs(phi2,phi0,sw0);
-p3=subs(phi3,phi0,sw0);
-p4=subs(phi4,phi0,sw0);
-u1x1=subs(u1x,phi0,sw0);
-u1y1=subs(u1y,phi0,sw0);
-u2x1=subs(u2x,phi0,sw0);
-u2y1=subs(u2y,phi0,sw0);
-u3x1=subs(u3x,phi0,sw0);
-u3y1=subs(u3y,phi0,sw0);
-u4x1=subs(u4x,phi0,sw0);
-u4y1=subs(u4y,phi0,sw0);
-
-d=[0;0;p0;u1x1;u1y1;p1;u2x1;u2y1;p2;u3x1;u3y1;p3;u4x1;u4y1;p4;];
+%% Mcdowell
+d=[u0x;u0y;psi0;u1x;u1y;psi1;u2x;u2y;psi2;u3x;u3y;psi3;u4x;u4y;psi4;];
 w=d'*K_OAtoD*d/(2*V);
-% d=[u1x1;u1y1;p1;u2x1;u2y1;p2;u3x1;u3y1;p3;u4x1;u4y1;p4;];
-% w=d'*K_AtoD*d/(2*V);
+
+%% Q
 c1=diff(w,e11,e11);c2=diff(w,e11,e22);c3=diff(w,e11,e12);
 c4=diff(w,e11,e21);c5=diff(w,e11,k13);c6=diff(w,e11,k23);
 c7=diff(w,e22,e22);c8=diff(w,e22,e12);c9=diff(w,e22,e21);
@@ -300,31 +218,5 @@ Q=[c1, c2, c3, c4, c5, c6;
    c6,c11,c15,c18,c20,c21];
 Q=double(Q);
 Q(abs(Q)<1e-3)=0;
-q = [c1, c2, c3 + c4, c3 - c4, c5, c6;
-   c2, c7, c8 + c9, c8 - c9, c10, c11;
-   (c3 + c4)/2, (c8 + c9)/2, (c12 + c16)/2 + c13, (c12 - c16)/2, (c14 + c17)/2, (c15 + c18)/2;
-   (c3 - c4)/2, (c8 - c9)/2, (c12 - c16)/2, (c12 + c16)/2 - c13, (c14 - c17)/2, (c15 - c18)/2;
-   c5, c10, c14 + c17, c14 - c17, c19, c20;
-   c6, c11, c15 + c18, c15 - c18, c20, c21];
-q=double(q);
-q(:,3:4)=q(:,3:4)/2;
-q(abs(q)<1e-5)=0;
-% q(4,:)=[];q(:,4)=[];
-qi=inv(q);
 
-C=Q(1:4,1:4);
-D=Q(5:6,5:6);
-B=Q(1:4,5:6);
-% Qi=inv(Q(1:4,1:4));
-Qi=inv(Q);
-Em=1/Qi(2,2);
-Er=Em/E;
-vm=-Qi(2,1)/Qi(1,1);
-Gm=1/(Qi(3,3)+Qi(3,4))/2;
-Bm=1/(Qi(1,1)+Qi(1,2))/2;
-yita1=(Qi(1,3)+Qi(1,4))/Qi(1,1);
-yita2=qi(3,1)/qi(1,1);
-zeta=qi(4,1)/qi(1,1);
-iota1=qi(5,1)/qi(1,1);
-iota2=qi(6,2)/qi(2,2)*a
 end
